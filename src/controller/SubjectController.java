3efdc9c039d857006299d8f5dcc3f83ff8946a9d
choi hyo jin <dkdlelrkdlrj@naver.com>
2025-06-10 09:46:30 +0900

    학생 , 과목 ,학과 를 오라클 DB와 연결해서 하는 프로그램

--- src/controller/SubjectController.java
+++ src/controller/SubjectController.java
@@ -4,138 +4,145 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import dao.SubjectDAO;
-import main.SubjectVO;
+import service.ConnectionService;
 import service.SubjectService;
 
 public class SubjectController {
 	public static Scanner scan = new Scanner(System.in);
-	//과목 목록
+
+	// 과목 목록
 	public void list() throws Exception {
 		SubjectDAO sd = new SubjectDAO();
+		SubjectService ss = new SubjectService();
+		ConnectionService connectionService = new ConnectionService();
+		
+		
 		System.out.println("과목 전체 리스트");
-		ArrayList<SubjectDVO> SubjectList = sd.selectAll();
-		if (SubjectList.size() == 0) {
+		ArrayList<SubjectDAO> SubDAOList = ss.selectSubAll(connectionService.getConn());
+		if (SubDAOList.size() == 0) {
 			System.out.println("과목전체리스트 내용이 없습니다.");
 			return;
-		} else if (SubjectList == null) {
+		} else if (SubDAOList == null) {
 			System.out.println("과목전체리스트 에러발생");
 			return;
 		}
-		for (SubjectVO data : SubjectList) {
+		for (SubjectDAO data : SubDAOList) {
 			System.out.println(data.toString());
 		}
 	}
-	
+
 	// 과목 입력
-		public void register() throws Exception {
-			Scanner scan = new Scanner(System.in);
-			SubjectDAO sd = new SubjectDAO();
-			SubjectVO svo = new SubjectVO();
-
-			String num; // 과목번호
-			String name; // 과목명
-
-			System.out.println("과목 정보 입력");
-			System.out.print("과목 번호 : ");
-			num = scan.nextLine();
-			System.out.print("과목명  : ");
-			name = scan.nextLine();
-			
-			svo.setNum(num);
-			svo.setName(name);
-
-			sd.insert(svo);
-			
-			System.out.println();
-			System.out.println("과목 전체 리스트");
-			list();
-			System.out.println();
-		}
-
-		// 과목 수정 관리
-		public void update() throws Exception {
-			Scanner scan = new Scanner(System.in);
-			
-			SubjectDAO dao = new SubjectService().selectStuOne(conn, 12);
-			System.out.println(dao);
-			
-			SubjectDAO sd = new SubjectDAO();
-			SubjectVO svo = new SubjectVO();
-
-			int no; // 일련번호
-			String num; // 과목 번호
-			String name; // 과목 이름
-
-			System.out.println("과목 전체 리스트");
-			list();
-			System.out.println();
-
-			System.out.println("수정할 과목번호 입력");
-			System.out.print("과목번호 : ");
-			num = scan.nextLine();
-
-
-			svo.setNo(no);
-			svo.setNum(num);
-			svo.setName(name);
-			int count = sd.update(svo);
-
-			if (count == 0) {
-				System.out.println("과목정보 수정 오류발생 조치바람");
-				return;
-			} else {
-				System.out.println("과목정보 수정완료");
-			}
-
-			System.out.println();
-			System.out.println("과목 전체 리스트");
-			list();
-			System.out.println();
-		}
+	public void register() throws Exception {
+		Scanner scan = new Scanner(System.in);
+		SubjectDAO sd = new SubjectDAO();
+		SubjectService ss = new SubjectService();
+		ConnectionService connectionService = new ConnectionService();
 		
-		// 과목 삭제 관리
-		public void delete() throws Exception {
-			Scanner scan = new Scanner(System.in);
+		String num; // 과목번호
+		String name; // 과목명
 
-			SubjectDAO sd = new SubjectDAO();
-			SubjectVO svo = new SubjectVO();
+		System.out.println("과목 정보 입력");
+		System.out.print("과목 번호 : ");
+		num = scan.nextLine();
+		System.out.print("과목명  : ");
+		name = scan.nextLine();
 
-			String num; // 일련번호
-			list();
-			System.out.println();
+		sd.setNum(num);
+		sd.setName(name);
 
-			System.out.println("삭제할 과목번호 입력");
-			System.out.print("과목번호 : ");
-			num = scan.nextLine();
-			svo.setNum(num);
-			int count = sd.deleteByNum(svo);
-			if (count == 0) {
-				System.out.printf("%s 번호 삭제 문제발생 조치바람\n", num);
-			} else {
-				System.out.printf("%s 번호 삭제성공 \n", num);
-			}
-			System.out.println();
-			System.out.println("과목 전체 리스트");
-			list();
-			System.out.println();
-		}
+		//ss.insert(num, name);
+
+		System.out.println();
+		System.out.println("과목 전체 리스트");
+		list();
+		System.out.println();
+	}
+
+	// 과목 수정 관리
+	public void update() throws Exception {
+		Scanner scan = new Scanner(System.in);
+
+		ConnectionService connectionService = new ConnectionService();
+		SubjectDAO dao = new SubjectService().selectSubOne(connectionService.getConn(), 12);
+		System.out.println(dao);
+
+		SubjectDAO sd = new SubjectDAO();
+		SubjectService ss = new SubjectService();
 		
-		// 과목 검색 관리
-		public static void Search() {
-			SubjectDAO sd = new SubjectDAO();
-			SubjectVO booksVO = null;
-			System.out.println("검색할 과목 'name'을 입력해주세요 : ");
-			String name = scan.nextLine();
-			booksVO = new BooksVO();
-			booksVO.setTitle(name);
-			
-			ArrayList<BooksVO> booksList = booksDAO.selectByTitle(booksVO);
-			if(booksList.size() <= 0 || booksList == null) {
-				System.out.println("책정보 검색에 오류가 발생했습니다.");
-				return;
-			}
-			for ( BooksVO data : booksList ) {
-				System.out.println(data.toString());
-			}
+		int no=0; // 일련번호
+		String num=""; // 과목 번호
+		String name=""; // 과목 이름
+
+		System.out.println("과목 전체 리스트");
+		list();
+		System.out.println();
+
+		System.out.println("수정할 과목번호 입력");
+		System.out.print("과목번호 : ");
+		num = scan.nextLine();
+
+		sd.setNo(no);
+		sd.setNum(num);
+		sd.setName(name);
+		boolean count = ss.update(connectionService.getConn(), sd);
+
+		if (count) {
+			System.out.println("과목정보 수정 오류발생 조치바람");
+			return;
+		} else {
+			System.out.println("과목정보 수정완료");
 		}
+
+		System.out.println();
+		System.out.println("과목 전체 리스트");
+		list();
+		System.out.println();
+	}
+
+	// 과목 삭제 관리
+	public void delete() throws Exception {
+		Scanner scan = new Scanner(System.in);
+
+		SubjectDAO sd = new SubjectDAO();
+		SubjectService ss = new SubjectService();
+		ConnectionService connectionService = new ConnectionService();
+		
+		String num; // 일련번호
+		list();
+		System.out.println();
+
+		System.out.println("삭제할 과목번호 입력");
+		System.out.print("과목번호 : ");
+		num = scan.nextLine();
+		sd.setNum(num);
+		boolean count = ss.delete(connectionService.getConn(),sd);
+		if (count) {
+			System.out.printf("%s 번호 삭제성공 \n", num);
+		} else {
+			System.out.printf("%s 번호 삭제 문제발생 조치바람\n", num);
+		}
+		System.out.println();
+		System.out.println("과목 전체 리스트");
+		list();
+		System.out.println();
+	}
+
+	// 과목 검색 관리
+	public static void Search() {
+		SubjectDAO sd = new SubjectDAO();
+		SubjectService ss = new SubjectService();
+		ConnectionService connectionService = new ConnectionService();
+		System.out.println("검색할 과목 'name'을 입력해주세요 : ");
+		String name = scan.nextLine();
+
+		
+		ArrayList<SubjectDAO> SubDAOList = ss.selectByName(connectionService.getConn(), name);
+		if (SubDAOList.size() <= 0 || SubDAOList == null) {
+			System.out.println("책정보 검색에 오류가 발생했습니다.");
+			return;
+		}
+		for (SubjectDAO data : SubDAOList) {
+			System.out.println(data.toString());
+		}
+	}
 }
